a7fc77ac635ce0d0bdfda1a5fda08d8f11f813bb
Leonardo Nunes <Leonardonunesaragao@gmail.com>
2021-03-13 01:14:07 -0300

    Adicionando modulo de testes e etapas

--- main.cpp
+++ main.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 #include <fstream>
-#include <sstream>
 #include "ArvoreAvl.h"
 #include "QuadTree.h"
 #include "ArvoreB.h"
@@ -29,12 +28,24 @@ using namespace std;
     
     Solução: (numero de 7 digitos -(numero de 6 digitos * 10) < 10 && >= 0)
 
+    Lembrar de avisar que são 5570 cidades;
+
+    Você deverá armazenar estes registros em uma tabela hash. Dimensione a tabela 
+    adequadamente e utilize uma função de hash que permita uma boa distribuição de 
+    chaves na tabela. Escolha também uma técnica para tratamento de colisões. 
+    Utilize como chave o par (código da cidade, data).
 
     ----- Trabalho Solicitado -----
 
     Etapa 1 - Ler brazil_cities_coordinates.csv e armazenar na QuadTree
 
-    Etapa 2 - 
+    Etapa 2 - Ler brazil_covid19_cities_processado.csv e armazenar na Tabela Hash
+
+    Etapa 3 - Implementação de estruturas de dados balanceada Arvore B e AVL
+    
+    Etapa 4 - Modulo de Testes
+
+    Etapa 5 - Analise das estruturas de dados balanceada
 
 */
 
@@ -42,7 +53,8 @@ using namespace std;
 void etapa1(QuadTree * aux){
 
     fstream file;
-    file.open("brazil_cities_coordinates.csv", fstream::in);
+    string name = "brazil_cities_coordinates.csv";
+    file.open(name, fstream::in);
 
     if(file.is_open()){
         
@@ -53,52 +65,126 @@ void etapa1(QuadTree * aux){
         while(!file.eof()){
 
             str = ' ';
-            Cidade aux;
+            Cidade c;
 
             getline(file, str,',');
-            aux.codigo_estado = stoi(str);
+            c.codigo_estado = stoi(str);
 
             getline(file, str,',');
-            aux.codigo_cidade = stoi(str);
+            c.codigo_cidade = stoi(str);
 
             getline(file, str,',');
-            aux.nome_cidade = str;
+            c.nome_cidade = str;
 
             getline(file, str,',');
-            aux.latitude = stod(str);
+            c.latitude = stod(str);
 
             getline(file, str,',');
-            aux.longitude = stod(str);
+            c.longitude = stod(str);
 
             getline(file, str);
             if(str == "FALSE")
-                aux.isEstado = 0;
+                c.isEstado = 0;
             else
-                aux.isEstado = 1;
+                c.isEstado = 1;
+
+            aux->inserir(c);
             
             count++;
 
-            /* Teste
-            cout<<aux.codigo_estado<<" "
-                <<aux.codigo_cidade<<" "
-                <<aux.nome_cidade<<" "
-                <<aux.latitude<<" "
-                <<aux.longitude<<" "
-                <<aux.isEstado<<endl;     
+            //Teste
+            /*
+            cout<<c.codigo_estado<<" "
+                <<c.codigo_cidade<<" "
+                <<c.nome_cidade<<" "
+                <<c.latitude<<" "
+                <<c.longitude<<" "
+                <<c.isEstado<<endl;     
 
             return;
             */
         }       
-        cout<<count<<" cidades lidas.";
+        cout<<count<<" cidades lidas."<<endl;
     }
     else{
-        cout<<"Erro: arquivo brazil_cities_coordinates.csv não encontrado!"<<endl;
+        cout<<"Erro: arquivo "<<name<<" não encontrado!"<<endl;
         exit(1);
     }
 }
 
-void etapa2(){
-    
+// Etapa 2 - Ler brazil_covid19_cities_processado.csv e armazenar na Tabela Hash
+void etapa2(TabelaHash * aux){
+
+    fstream file;
+    string name = "brazil_covid19_cities_processado.csv";
+    file.open(name, fstream::in);
+
+    if(file.is_open()){
+        
+        string str;
+        getline(file, str); // para nao pegar a primeira linha
+        int count = 0;
+
+        while(!file.eof()){
+
+            str = ' ';
+            InfoCidade c;
+
+            getline(file, str,',');
+            c.setData(str);
+
+            getline(file, str,',');
+            c.setSiglaEstado(str);
+
+            getline(file, str,',');
+            c.setNomeCidade(str);
+
+            getline(file, str,',');
+            c.setCodigoCidade(stoi(str));
+
+            getline(file, str,',');
+            c.setCasos(stoi(str));
+
+            getline(file, str);
+            c.setMortes(stoi(str));
+
+            aux->add(c);
+
+            count++;
+            cout<<count<<" registros lidos."<<endl;
+            // Teste
+            /*
+            cout<<c.getData()<<" "
+                <<c.getSiglaEstado()<<" "
+                <<c.getNomeCidade()<<" "
+                <<c.getCodigoCidade()<<" "
+                <<c.getCasos()<<" "
+                <<c.getMortes()<<endl;     
+
+            return;
+            */
+        }       
+        cout<<count<<" registros lidos.";
+    }
+    else{
+        cout<<"Erro: arquivo "<<name<<" não encontrado!"<<endl;
+        exit(1);
+    }
+}
+
+// Etapa 3 - Implementação de estruturas de dados balanceada
+void etapa3(){
+
+}
+
+// Etapa 4 - Modulo de Testes
+void etapa4(){
+
+}
+
+// Etapa 5 - Analise das estruturas de dados balanceada
+void etapa5(){
+
 }
 
 void menuSelecionado(char a)
@@ -115,26 +201,10 @@ void menuSelecionado(char a)
     }
     case '3':
     {        
-        //N = 1000, 5000, 10000, 50000, 100000 e 500000
-        int n [4] = {1000, 5000, 10000, 50000}; 
-        
-        cout << "\nAguarde... \n";
-
-        for(int i = 0; i<4; i++){
-            for(int j = 0; j < 5;j ++){
-                for(int k = 0; k <3 ; k ++){
-                
-                }
-            }
-        }
-
-        cout << "\nFinalizado! \n";
         break;
     }
-    //---------- Usar para Testes ----------
     case '4':
     {
-        cout << "\nSem testes por aqui no momento. :(" << endl;
         break;
     }
     default:
@@ -148,24 +218,22 @@ void menu()
     //System("tput reset");
     char menu;
     while (true)
-    {
+    {   
         cout << endl;
-        cout << "\n# Menu principal #" << endl;
-        cout << "[1] - Parte 1" << endl;
-        cout << "[2] - Parte 2" << endl;
-        cout << "[3] - Parte 3" << endl;
-        cout << "[4] - Parte 4" << endl;
+        cout << "[1] - Inserção de N cidades na quad tree" << endl;
+        cout << "[2] - Inserção de N registros na tabela hash" << endl;
+        cout << "[3] - Inserção de N chaves na árvore AVL" << endl;
+        cout << "[4] - Inserção de N chaves na árvore B" << endl;
         cout << endl;
         do
         {
-            cout << "Digite uma opcao do menu: ";
+            cout << "Digite uma opcao do menu ou 'q' para continuar: ";
             cin >> menu;
         } while (((menu < '0' || menu > '5') && (menu < 'a' || menu > 'z')) && menu != 'q');
 
         if (menu == 'q')
         {
             cout << "Saindo..." << endl;
-            cout << "Bye" << endl;
             return;
         }
         menuSelecionado(menu);
@@ -178,10 +246,25 @@ int main(){
 
     cout<<"Etapa 1"<<endl;
     cout<<"Lendo arquivo brazil_cities_coordinates.csv ..."<<endl;
+
     QuadTree * a = new QuadTree();
     etapa1(a);
 
+    cout<<"\nEtapa 2"<<endl;
+    cout<<"Lendo arquivo brazil_covid19_cities_processado.csv ..."<<endl;
+    
+    TabelaHash * tabela = new TabelaHash(1431490*5); //Ver qual sondagem usar
+    etapa2(tabela);
+
+    cout<<"\nEtapa 3"<<endl;
+    etapa3();
+
+    cout<<"\nEtapa 4"<<endl;
+    cout<<"Modulo de Testes"<<endl;
     menu();
+
+    cout<<"\nEtapa 5"<<endl;
+    etapa5();
     
     return 0;
 }
